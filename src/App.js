24ee3e2e7995f32407cd9a1596edb5428136ecc8
Sahoo <soumyaranjan.b.sahoo@accenture.com>
2022-03-28 20:26:45 +0530

    fixing bugs

--- src/App.js
+++ src/App.js
@@ -6,44 +6,41 @@ import Spinners from "./Spinners";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 function App() {
-  
-
   const [posts, setposts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerpage, setPostsPerpage] = useState(15);
+  const [postsPerpage, setPostsPerpage] = useState(10);
   const [addPost, setAddPost] = useState([]);
-  const fetchPosts = async () => {
-    setLoading(true);
-    const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
-    setposts(res.data);
-    setLoading(false);
-    setCurrentPage(currentPage + 1);
-  };
- 
+
   useEffect(() => {
-    fetchPosts().then(()=>{
-      setAddPost(posts.slice(0, 10))
-    })
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setposts(res.data);
+      setLoading(false);
+      // setCurrentPage(currentPage + 1);
+    };
+    fetchPosts();
   }, []);
- 
+
   const indexOfLastPost = currentPage * postsPerpage;
   const indexOfFirstPost = indexOfLastPost - postsPerpage;
   const currentPost = posts.slice(indexOfFirstPost, indexOfLastPost);
+  useEffect((item) => {
+    item + 1;
+  });
 
-
-  useEffect(() => {
-    setAddPost([...addPost,...currentPost])
-  }, []);
-
+  // useEffect(() => {
+  //   setAddPost([...addPost,...currentPost])
+  // }, []);
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-  const fetchMoreData=()=>{
-    setCurrentPage(currentPage+1)
-      fetchPosts();
-  }
-  console.log(currentPost)
+  // const fetchMoreData=()=>{
+  //   setCurrentPage(currentPage+1)
+  //     fetchPosts();
+  // }
+  console.log(currentPost);
   if (loading) {
     return <Spinners />;
   }
@@ -51,7 +48,7 @@ function App() {
     <div className="App">
       <h1>Pagination</h1>
       <div id="scrollableDiv" style={{ height: 300, overflow: "auto" }}>
-        <InfiniteScroll
+        {/* <InfiniteScroll
           dataLength={posts.length}
           next={fetchMoreData}
           hasMore={true}
@@ -62,13 +59,16 @@ function App() {
           //   </p>
           // }
           scrollableTarget="scrollableDiv"
-        >
-          {addPost && addPost.map((post) => (
-            <p key={post.id}>{post.title}</p>
-          ))}
-        </InfiniteScroll>
+        > */}
+        {currentPost &&
+          currentPost.map((post) => <p key={post.id}>{post.title}</p>)}
+        {/* </InfiniteScroll> */}
       </div>
-      {/* <Pagination postsPerPage={postsPerpage} totalPosts={posts.length} paginate={paginate}/> */}
+      <Pagination
+        postsPerPage={postsPerpage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
     </div>
   );
 }
